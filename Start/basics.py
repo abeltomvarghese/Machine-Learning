2b9b7f996992d5b6383388162120a669fc49ff26
Abel Tom Varghese <abeltomvarghese1998@gmail.com>
2018-07-01 13:29:45 +0100

    restructured approach

--- Start/basics.py
+++ Start/basics.py
@@ -1,9 +1,12 @@
 import pandas as pd
 filePath = "dataset/house-prices/train.csv"
 house_data = pd.read_csv(filePath)
-print(house_data.columns)
+print(house_data.columns)  #prints a list of columns
 price_column = house_data.SalePrice
-print(price_column.head())
+print(price_column)
 columns_of_interest = ["GarageCond","Electrical"]
 columns_of_data = house_data[columns_of_interest]
+pool = house_data.PoolQC
+pool = pool.dropna()
+print(pool)
 print(columns_of_data.describe())
